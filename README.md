9cdf3992e0ede2b2b584e54c59d0f2b4b2a2c705
Jason Long <jaslong@opendoor.com>
2021-11-28 14:30:13 -0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# react-native-android-intent
+
+Get access to the Android Activity's intent
+
+## Installation
+
+```sh
+npm install react-native-android-intent
+```
+
+## Usage
+
+```js
+import { multiply } from "react-native-android-intent";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/reactnativeandroidintent/AndroidIntentPackage.kt
@@ -0,0 +1,17 @@
+package com.reactnativeandroidintent
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class AndroidIntentPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(AndroidIntentModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
